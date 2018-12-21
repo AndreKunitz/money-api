37c3c4122c9f7754f4fc500c9b02401bbff41fbc
André Kunitz <andrekunitz@gmail.com>
2018-12-20 22:12:28 -0200

    Retornando 404 caso não exista a Categoria

--- src/main/java/com/andrekunitz/money/api/resource/CategoriaResource.java
+++ src/main/java/com/andrekunitz/money/api/resource/CategoriaResource.java
@@ -2,11 +2,15 @@ package com.andrekunitz.money.api.resource;
 
 import com.andrekunitz.money.api.model.Categoria;
 import com.andrekunitz.money.api.repository.CategoriaRepository;
+import org.aspectj.bridge.MessageUtil;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import javax.servlet.http.HttpServletResponse;
+import java.net.URI;
 import java.util.List;
 
 @RestController
@@ -21,5 +25,22 @@ public class CategoriaResource {
         return categoriaRepository.findAll();
     }
 
+    @PostMapping
+    public ResponseEntity<Categoria> criar(@RequestBody Categoria categoria, HttpServletResponse response) {
+        Categoria categoriaSalva = categoriaRepository.save(categoria);
 
+        URI uri = ServletUriComponentsBuilder.fromCurrentRequestUri()
+                .path("/{codigo}").buildAndExpand(categoriaSalva.getCodigo()).toUri();
+
+        response.setHeader("Location", uri.toASCIIString());
+
+        return ResponseEntity.created(uri).body(categoriaSalva);
+    }
+
+    @GetMapping("/{codigo}")
+    public ResponseEntity<Categoria> buscarPeloCodigo(@PathVariable Long codigo) {
+        Categoria categoria = categoriaRepository.findById(codigo).orElse(null);
+
+        return categoria != null ? ResponseEntity.ok(categoria) : ResponseEntity.notFound().build();
+    }
 }
