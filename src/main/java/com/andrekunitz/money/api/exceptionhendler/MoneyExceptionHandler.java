19c2cbf7cbba5940c6aded4b04eba791f9ae8ec5
André Kunitz <andre@DESKTOP-OMVL3VM.localdomain>
2019-01-04 13:14:09 +0000

    Removendo Pessoa com DELETE

--- src/main/java/com/andrekunitz/money/api/exceptionhendler/MoneyExceptionHandler.java
+++ src/main/java/com/andrekunitz/money/api/exceptionhendler/MoneyExceptionHandler.java
@@ -1,9 +1,9 @@
 package com.andrekunitz.money.api.exceptionhendler;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.MessageSource;
 import org.springframework.context.i18n.LocaleContextHolder;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -12,13 +12,13 @@ import org.springframework.validation.BindingResult;
 import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
-import java.util.Locale;
 
 @ControllerAdvice
 public class MoneyExceptionHandler extends ResponseEntityExceptionHandler {
@@ -42,6 +42,15 @@ public class MoneyExceptionHandler extends ResponseEntityExceptionHandler {
         return handleExceptionInternal(ex, erros, headers, HttpStatus.BAD_REQUEST, request);
     }
 
+    @ExceptionHandler( {EmptyResultDataAccessException.class} )
+    public ResponseEntity<Object> handleEmptyResultDataAccessException(EmptyResultDataAccessException ex, WebRequest request) {
+        String menssagemUsuario = messageSource.getMessage("recurso.nao-encontrado", null, LocaleContextHolder.getLocale());
+        String menssagemDesenvolvedor = ex.toString();
+        List<Erro> erros = Arrays.asList(new Erro(menssagemUsuario, menssagemDesenvolvedor));
+
+        return handleExceptionInternal(ex, erros, new HttpHeaders(), HttpStatus.NOT_FOUND, request);
+    }
+
     private List<Erro> criarListaDeErros(BindingResult bidingResult) {
         List<Erro> erros = new ArrayList<>();
 
@@ -53,7 +62,6 @@ public class MoneyExceptionHandler extends ResponseEntityExceptionHandler {
 
         return erros;
     }
-
 
     public static class Erro {
 
--- src/main/java/com/andrekunitz/money/api/resource/PessoaResource.java
+++ src/main/java/com/andrekunitz/money/api/resource/PessoaResource.java
@@ -44,4 +44,10 @@ public class PessoaResource {
         return pessoaProcurada != null ? ResponseEntity.ok(pessoaProcurada) : ResponseEntity.notFound().build();
     }
 
+    @DeleteMapping("/{codigo}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void remover(@PathVariable Long codigo) {
+        pessoaRepository.deleteById(codigo);
+    }
+
 }
