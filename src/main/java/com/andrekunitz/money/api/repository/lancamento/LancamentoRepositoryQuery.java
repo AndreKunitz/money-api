57395bfe0a4463c630cf7b304d2e66b041403987
Andre <andrekunitz@gmail.com>
2019-01-22 17:39:37 +0000

    Implementando projeção de Lançamento

--- src/main/java/com/andrekunitz/money/api/repository/lancamento/LancamentoRepositoryQuery.java
+++ src/main/java/com/andrekunitz/money/api/repository/lancamento/LancamentoRepositoryQuery.java
@@ -2,10 +2,12 @@ package com.andrekunitz.money.api.repository.lancamento;
 
 import com.andrekunitz.money.api.model.Lancamento;
 import com.andrekunitz.money.api.repository.filter.LancamentoFilter;
+import com.andrekunitz.money.api.repository.projection.ResumoLancamento;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 
 public interface LancamentoRepositoryQuery {
 
     public Page<Lancamento> filtrar(LancamentoFilter lancamentoFilter, Pageable pageable);
+    public Page<ResumoLancamento> resumir(LancamentoFilter lancamentoFilter, Pageable pageable);
 }
--- src/main/java/com/andrekunitz/money/api/resource/LancamentoResource.java
+++ src/main/java/com/andrekunitz/money/api/resource/LancamentoResource.java
@@ -5,6 +5,7 @@ import com.andrekunitz.money.api.exceptionhendler.MoneyExceptionHandler;
 import com.andrekunitz.money.api.model.Lancamento;
 import com.andrekunitz.money.api.repository.LancamentoRepository;
 import com.andrekunitz.money.api.repository.filter.LancamentoFilter;
+import com.andrekunitz.money.api.repository.projection.ResumoLancamento;
 import com.andrekunitz.money.api.service.LancamentoService;
 import com.andrekunitz.money.api.service.exception.PessoaInexistenteOuInativaExcepton;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -43,6 +44,12 @@ public class LancamentoResource {
     @PreAuthorize("hasAuthority('ROLE_PESQUISAR_LANCAMENTO') and #oauth2.hasScope('read')")
     public Page<Lancamento> pesquisar(LancamentoFilter lancamentoFilter, Pageable pageable) {
         return lancamentoRepository.filtrar(lancamentoFilter, pageable);
+    }
+
+    @GetMapping(params = "resumo")
+    @PreAuthorize("hasAuthority('ROLE_PESQUISAR_LANCAMENTO') and #oauth2.hasScope('read')")
+    public Page<ResumoLancamento> resumir(LancamentoFilter lancamentoFilter, Pageable pageable) {
+        return lancamentoRepository.resumir(lancamentoFilter, pageable);
     }
 
     @GetMapping("/{codigo}")
