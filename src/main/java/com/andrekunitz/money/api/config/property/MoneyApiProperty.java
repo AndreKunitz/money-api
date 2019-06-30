0984ffe3bc1dcf90712e34c71258d66f71d06501
André Kunitz <andrekunitz@gmail.com>
2019-06-30 13:38:51 -0300

    Release v1.0.0

--- src/main/java/com/andrekunitz/money/api/config/property/MoneyApiProperty.java
+++ src/main/java/com/andrekunitz/money/api/config/property/MoneyApiProperty.java
@@ -5,7 +5,7 @@ import org.springframework.boot.context.properties.ConfigurationProperties;
 @ConfigurationProperties("money")
 public class MoneyApiProperty {
 
-    private String origemPermitida = "http://localhost:4200";
+    private String origemPermitida = "https://andremoney-ui.herokuapp.com";
 
     private final Seguranca seguranca = new Seguranca();
 
