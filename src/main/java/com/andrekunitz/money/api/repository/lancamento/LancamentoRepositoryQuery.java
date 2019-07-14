13d011be7ace13fb1a8a547b7f5874bb4ab5dd57
André Kunitz <andrekunitz@gmail.com>
2019-07-14 14:38:20 -0300

    Create query to return data by Categoria

--- src/main/java/com/andrekunitz/money/api/repository/lancamento/LancamentoRepositoryQuery.java
+++ src/main/java/com/andrekunitz/money/api/repository/lancamento/LancamentoRepositoryQuery.java
@@ -1,12 +1,18 @@
 package com.andrekunitz.money.api.repository.lancamento;
 
+import com.andrekunitz.money.api.dto.LancamentoEstatisticaCategoria;
 import com.andrekunitz.money.api.model.Lancamento;
 import com.andrekunitz.money.api.repository.filter.LancamentoFilter;
 import com.andrekunitz.money.api.repository.projection.ResumoLancamento;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 
+import java.time.LocalDate;
+import java.util.List;
+
 public interface LancamentoRepositoryQuery {
+
+    public List<LancamentoEstatisticaCategoria> porCategoria(LocalDate mesReferencia);
 
     public Page<Lancamento> filtrar(LancamentoFilter lancamentoFilter, Pageable pageable);
     public Page<ResumoLancamento> resumir(LancamentoFilter lancamentoFilter, Pageable pageable);
