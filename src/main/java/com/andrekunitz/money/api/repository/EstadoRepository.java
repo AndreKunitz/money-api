d4b543cb5f73f009dc45b75f19a0ff3ad9de1a92
André Kunitz <andrekunitz@gmail.com>
2019-08-14 09:05:48 -0300

    Create repository and resource for city and state entities

---
+++ src/main/java/com/andrekunitz/money/api/repository/EstadoRepository.java
@@ -0,0 +1,7 @@
+package com.andrekunitz.money.api.repository;
+
+import com.andrekunitz.money.api.model.Estado;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface EstadoRepository extends JpaRepository<Estado, Long> {
+}
---
+++ src/main/java/com/andrekunitz/money/api/resource/CidadeResource.java
@@ -0,0 +1,26 @@
+package com.andrekunitz.money.api.resource;
+
+import com.andrekunitz.money.api.model.Cidade;
+import com.andrekunitz.money.api.repository.CidadeRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/cidades")
+public class CidadeResource {
+
+    @Autowired
+    private CidadeRepository cidadeRepository;
+
+    @GetMapping
+    @PreAuthorize("isAuthenticated()")
+    public List<Cidade> pesquisar(@RequestParam Long estado) {
+        return  cidadeRepository.findByEstadoCodigo(estado);
+    }
+}
